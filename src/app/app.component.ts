169c9b655ed69261484843b068509c0ca655605c
Amarnath Reddy Dornala <amar@dornala.me>
2019-08-07 00:35:53 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {model} from './protos/model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'protoApp';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const collection = model.Collection.create({
+      id: '1',
+      name: 'Amarnath',
+      description: 'Sample Name'
+    });
+    console.log(model.Collection.fromObject(collection));
+  }
 }
